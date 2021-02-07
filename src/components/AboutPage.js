fc5a305e5f02a4f2a1e79f5bca2b1535969d1905
drisdon <drisdon07@gmail.com>
2021-02-07 14:20:27 -0500

    Update bio and picture

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,24 +7,23 @@ class AboutPage extends Component {
     return (<div><NavBar currentPage='about'/>
       <h1>ABOUT THE ARTIST</h1>
       <div className='about-me'>
-      <img className='headshot' src='https://res.cloudinary.com/dczipv49x/image/upload/v1558388280/FullSizeRender_wxewcj.jpg' alt="Izzy"/>
+      <img className='headshot' src='https://res.cloudinary.com/dczipv49x/image/upload/v1612716069/bio_wqodat.jpg' alt="Izzy"/>
       <p className='bio'>
-      Izzy is an artist currently based in Boulder, Colorado. She started her tattoo career at Intimate Body Art Studios outside of Philadelphia,
-      Pennsylvania in January of 2017, and has been loving it every since. She moved to Boulder the summer of 2019, and is currently tattooing at
-      Junkyard Ink in Louisville. While she has experience with many different styles, subjects, and skin types, Izzy primarily enjoys tattooing
-      botanical and nature tattoos in a neotraditional style.</p>
+      Izzy is an artist based at Junkyard Ink in Louisville, Colorado, a ten minute drive outside of Boulder. She started her tattoo career at Intimate Body Art Studios
+      outside of Philadelphia, Pennsylvania in January of 2017, and moved to Boulder the summer of 2019. While she has experience with many different styles, subjects,
+      and skin types, Izzy primarily enjoys tattooing botanical and nature tattoos in a neotraditional style. She loves the challenge of coverups, and hopes to provide
+      complimentary scar coverups in the future. For Izzy, tattoo art is about body positivity, artistic expression, and self love. The support she gives her clients in
+      helping them love themselves and their bodies a little bit more is the most fulfilling part of her job.</p>
       <p className='bio'>
-      Aside from tattooing, Izzy has been an artist for as long as she can remember,
-      and has been lucky enough to work in a wide range of mediums such as acrylic
-      paint, polymer clay, colored pencils, watercolor, wood, and fiber media, and have a
-      plethora of art-related jobs. She’s been a studio assistant to a puppet maker and a
-      glass artist, has worked at UPenn’s Institute of Contemporary Art on their installation
-      crew, and has done prop, set design, and carpentry work in theaters in three different states. In
-      addition to tattooing, she currently works at a cat exclusive veterinary hospital and figure models in her spare time.</p>
+      Aside from tattooing, Izzy has been an artist for as long as she can remember, and has been lucky enough to work in a wide range of
+      mediums such as acrylic paint, gouache, colored pencils, watercolor, wood, polymer clay, and fiber media. She’s been a studio assistant
+      to a puppet maker and a glass artist, has worked at UPenn’s Institute of Contemporary Art on their installation crew, and has done prop,
+      set design, and carpentry work in theaters in three different states. In addition to tattooing, she is a practicing witch who reads
+      tarot and uses magic as self care. She frequently shares aspects of her practice on her Instagram, <a href="https://www.instagram.com/isabump/" target="_blank">@isabump</a>.</p>
        <p className='bio'>
-       Izzy is always interested in learning more about the art she has access to, and one of her favorite
+       Izzy is always interested in learning more about the art she has access to. One of her favorite
        things about tattooing is learning the stories and science behind the subjects her clients give her.
-       She lives with her boyfriend Junji, and their two cats, little Lily and chirpy Ralph.
+       She lives with her boyfriend Junji, and their two cats, Lily and Ralph (who you may see tattooed on her shoulder if you pay her a visit!).
        </p>
       </div>
     </div>);
